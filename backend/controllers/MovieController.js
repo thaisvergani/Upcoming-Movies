911e663ab2b7f0ecb9f243026af8985e4a9804ed
Thais Vergani <thais.vergani@gmail.com>
2019-09-13 20:13:15 -0300

    Added infinite scroller

--- backend/controllers/MovieController.js
+++ backend/controllers/MovieController.js
@@ -2,19 +2,23 @@ const axios = require('axios');
 const token = "1f54bd990f1cdfb230adb312546d765d";
 
 module.exports = {
-   
-    async list_movies(req, res){
-    
+
+    async list_movies(req, res) {
+
         const response = await axios.get(
             'https://api.themoviedb.org/3/movie/upcoming',
             {
-               params: { page: '1',
-                language: 'en-US',
-                api_key: token },
+                params: {
+                    page: req.query.page,
+                    language: 'en-US',
+                    api_key: token
+                },
             }
         )
-        console.log(response.data.results);
-                    
+        response.data.results.forEach((item) => { 
+            item.poster_path = `http://image.tmdb.org/t/p/w92/`.concat( item.poster_path);
+        })
         return res.json(response.data.results);
     }
+
 };
--- frontend/src/components/movies.js
+++ frontend/src/components/movies.js
@@ -1,29 +1,78 @@
 import React, { Component } from 'react';
 import './movies.css';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Movies extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      movies: []
+      movies: [],
+      hasMoreItems: true,
+      page: 1,
+      items: []
     };
   };
 
-  componentDidMount() {
-    fetch('/api/movies')
+  
+  loadItems(page) {
+    let url = '/api/movies';
+    
+    let options = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        page: this.state.page
+      },
+      
+    };
+
+    fetch(`/api/movies?page=${encodeURIComponent( this.state.page)}`)
       .then(res => res.json())
-      .then(movies => this.setState({movies}, () => console.log('movies fetched...', movies)));
+      .then(movies => {
+        this.setState({
+          movies: movies,
+          hasMoreItems: true,
+          page: page + 1
+
+        });
+        console.log(movies[0], this.state.page)
+      }); 
   }
 
   render() {
+
+    const loader = <div className="loader">Loading ...</div>;
+
+    this.state.movies.map((movie, i) => {
+
+      this.state.items.push(
+        <div className="movie" key={movie.id}>
+          <img  src={movie.poster_path}/>
+          <div className="movie-title">
+           <p >{movie.title}</p>
+            </div>
+        </div>
+
+      );
+
+    });
+
     return (
-      <div>
+      <div className="movies-wrapper">
         <h2>Movies</h2>
-        <ul>
-        {this.state.movies.map(movie => 
-          <li key={movie.id}>{movie.title} {movie.genre}</li>
-        )}
-        </ul>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadItems.bind(this)}
+          hasMore={this.state.hasMoreItems}
+          loader={loader}>
+
+          <div className="tracks">
+            {this.state.items}
+          </div>
+        </InfiniteScroll>
+
       </div>
     );
   }
